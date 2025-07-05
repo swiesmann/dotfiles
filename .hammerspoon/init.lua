f2e73ddf8681a963929ee9ec6bb639c29300ac71
Simon Wiesmann <simon.wiesmann@mailbox.org>
2025-07-05 23:33:11 +0200

    add hammerspoon config

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,10 +1,12 @@
+local hyperKey = {'cmd', 'alt', 'ctrl', 'shift'}
+
 hyperKeyShortcuts = {
     {"1", "Brave Browser"},
     {"2", "IntelliJ IDEA Ultimate"},
 }
 
 for i,shortcut in ipairs(hyperKeyShortcuts) do
-    hs.hotkey.bind({"cmd","ctrl","option","shift"}, shortcut[1], function()
+    hs.hotkey.bind(hyperKey, shortcut[1], function()
         hs.application.launchOrFocus(shortcut[2])
     end)
 end
