bd5c53d26108109625d9ea8ce5c9fe5d7f337f5a
Simon Wiesmann <simon.wiesmann@mailbox.org>
2025-07-19 19:14:16 +0200

    add macpass to hammerspoon

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -1,12 +1,16 @@
-local hyperKey = {'cmd', 'alt', 'ctrl', 'shift'}
+local hyperKey = { 'cmd', 'alt', 'ctrl', 'shift' }
 
 hyperKeyShortcuts = {
-    {"1", "Brave Browser"},
-    {"2", "IntelliJ IDEA Ultimate"},
+    { "1", "Brave Browser" },
+    { "2", "IntelliJ IDEA Ultimate" },
+    { "3", "MacPass" },
+    { "4", "/Applications/Obsidian.app" },
+    --    anytype, signal VS teams, reaper
 }
 
-for i,shortcut in ipairs(hyperKeyShortcuts) do
+for i, shortcut in ipairs(hyperKeyShortcuts) do
     hs.hotkey.bind(hyperKey, shortcut[1], function()
         hs.application.launchOrFocus(shortcut[2])
+        --hs.application.launchOrFocusByBundleID(shortcut[2])
     end)
 end
