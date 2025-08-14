0cc30677404a8eef111c3747258c8c17317eefe5
Simon Wiesmann <simon.wiesmann@otto.de>
2025-08-14 12:35:11 +0200

    add basic debug log to my functions

--- .local/bin/my-zsh-functions.sh
+++ .local/bin/my-zsh-functions.sh
@@ -1,11 +1,24 @@
+function debug() {
+    if [[ "$MY_ZSH_FUNCTIONS_DEBUG" == "true" ]]; then
+        echo "$(date) ${funcstack[-1]} $@" >> /tmp/my-zsh-functions-debug.log
+    fi
+}
+
 function source_if_exists() {
     if [[ -f "$1" ]]; then
+        debug "sourcing $1"
         source "$1"
+    else
+        debug "file does not exist: $1"
     fi
+
 }
 
 function path_if_exists() {
     if [[ -d "$1" ]]; then
+        debug "adding to PATH: $1"
         export PATH="$1:$PATH"
+    else
+        debug "folder does not exist: $1"
     fi
 }
